579fd30c8ee302fd7cd3367f57ec883b50f76490
Sarah <sarahbezerra2013@gmail.com>
2021-12-06 18:27:18 -0300

    Otimizando caminho dos stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,20 +1,20 @@
 export default function Stories() {
     const infos_storie = [
-        {src:"assets/img/9gag.svg", usuario:"9gag"},
-        {src:"assets/img/meowed.svg", usuario:"meowed"},
-        {src:"assets/img/barked.svg", usuario:"barked"},
-        {src:"assets/img/nathanwpylestrangeplanet.svg", usuario:"nathanwpylestrangeplanet"},
-        {src:"assets/img/wawawicomics.svg", usuario:"wawawicomics"},
-        {src:"assets/img/respondeai.svg", usuario:"respondeai"},
-        {src:"assets/img/filomoderna.svg", usuario:"filomoderna"},
-        {src:"assets/img/memeriagourmet.svg", usuario:"memeriagourmet"}
+        {src:"9gag.svg", usuario:"9gag"},
+        {src:"meowed.svg", usuario:"meowed"},
+        {src:"barked.svg", usuario:"barked"},
+        {src:"nathanwpylestrangeplanet.svg", usuario:"nathanwpylestrangeplanet"},
+        {src:"wawawicomics.svg", usuario:"wawawicomics"},
+        {src:"respondeai.svg", usuario:"respondeai"},
+        {src:"filomoderna.svg", usuario:"filomoderna"},
+        {src:"memeriagourmet.svg", usuario:"memeriagourmet"}
     ];
 
     return (
         infos_storie.map(infos => 
             <div class="story">
                 <div class="imagem">
-                    <img src={infos.src} />
+                    <img src={`assets/img/${infos.src}`} />
                 </div>
                 <div class="usuario">
                 {infos.usuario}
