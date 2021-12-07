a3a445f1ec3a793f74e24f3753e903945a690762
Sarah <sarahbezerra2013@gmail.com>
2021-12-07 16:46:20 -0300

    otimizando componente dos Stories

--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -7,12 +7,7 @@ export default function Corpo() {
     return (
       <div class="corpo">
         <div class="esquerda">
-          <div class="stories">
             <Stories />
-            <div class="setinha">
-              <ion-icon name="chevron-forward-circle"></ion-icon>
-            </div>
-          </div>
 
           <div class="posts">
             <Posts />
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,27 +1,36 @@
 export default function Stories() {
     const infos_storie = [
-        {src:"9gag.svg", usuario:"9gag"},
-        {src:"meowed.svg", usuario:"meowed"},
-        {src:"barked.svg", usuario:"barked"},
-        {src:"nathanwpylestrangeplanet.svg", usuario:"nathanwpylestrangeplanet"},
-        {src:"wawawicomics.svg", usuario:"wawawicomics"},
-        {src:"respondeai.svg", usuario:"respondeai"},
-        {src:"filomoderna.svg", usuario:"filomoderna"},
-        {src:"memeriagourmet.svg", usuario:"memeriagourmet"}
+        {imagem:"9gag.svg", usuario:"9gag"},
+        {imagem:"meowed.svg", usuario:"meowed"},
+        {imagem:"barked.svg", usuario:"barked"},
+        {imagem:"nathanwpylestrangeplanet.svg", usuario:"nathanwpylestrangeplanet"},
+        {imagem:"wawawicomics.svg", usuario:"wawawicomics"},
+        {imagem:"respondeai.svg", usuario:"respondeai"},
+        {imagem:"filomoderna.svg", usuario:"filomoderna"},
+        {imagem:"memeriagourmet.svg", usuario:"memeriagourmet"}
     ];
 
     return (
-        infos_storie.map(infos => 
-            <div class="story">
-                <div class="imagem">
-                    <img src={`assets/img/${infos.src}`} />
-                </div>
-                <div class="usuario">
-                {infos.usuario}
-                </div>
+        <div className="stories">
+            {infos_storie.map((story) => <Story imagem={story.imagem} usuario={story.usuario} />)}
+            
+            <div class="setinha">
+              <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
-        )
-      
-    );
+        </div>
+    )
+}
 
+
+function Story(props) {
+    return (
+        <div class="story">
+            <div class="imagem">
+                <img src={`assets/img/${props.imagem}`} />
+            </div>
+            <div class="usuario">
+            {props.usuario}
+            </div>
+        </div>
+    );
 }
